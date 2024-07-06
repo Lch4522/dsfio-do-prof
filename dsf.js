43205b9046085cc2b991b720773b5f9b20456a3b
Lch4522 <aiko6846@gmail.com>
2024-07-06 09:24:52 -0300

    meu projeto

---
+++ dsf.js
@@ -0,0 +1,68 @@
+let nomeHeroi = ""
+let expHeroi = 20000
+let rankHeroi = ""
+let lvl = 1
+
+const nm = "parabens, voce se tornou um "
+
+for(let i = 0; i < 1; i++){
+
+console.log(" LEVEL UP!!!")
+
+}
+
+if(expHeroi < 1000){
+nomeHeroi = "freshman"
+rankHeroi = "ferro"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+if(expHeroi >= 1001 && expHeroi <= 2000){
+nomeHeroi = "gladiador"
+rankHeroi = "bronze"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+ if(expHeroi >= 2001 && expHeroi <= 5000){
+nomeHeroi = "ghost"
+rankHeroi = "prata"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+if(expHeroi >= 5001 && expHeroi <= 7000){
+nomeHeroi = "flash"
+rankHeroi =  "ouro"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+if(expHeroi >= 7001 && expHeroi <= 8000){
+nomeHeroi = "sensei"
+rankHeroi = "platina"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+if(expHeroi >= 8001 && expHeroi <= 9000){
+nomeHeroi = "king of seas"
+rankHeroi = "ascendente"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+if(expHeroi >= 9001 && expHeroi <= 10000){
+nomeHeroi = "king of stars"
+rankHeroi = "imortal"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
+
+if(expHeroi >= 10001){
+nomeHeroi = "Black Hole"
+rankHeroi = "radiante"
+console.log(nm + nomeHeroi)
+console.log("o heroi " + nomeHeroi , "esta no rank: " + rankHeroi)
+}
